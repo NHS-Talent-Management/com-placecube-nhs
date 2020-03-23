4124ec3acaaf3975c0ba6418e748d9ff6f324c1c
ilenia.zedda <ilenia.zedda@placecube.com>
2020-03-23 17:26:05 +0000

    Css class cleanup

--- com.placecube.nhs.talentdashboard.web/src/main/java/com/placecube/nhs/talentdashboard/web/portlet/TalentDashboardPortlet.java
+++ com.placecube.nhs.talentdashboard.web/src/main/java/com/placecube/nhs/talentdashboard/web/portlet/TalentDashboardPortlet.java
@@ -7,7 +7,7 @@ import org.osgi.service.component.annotations.Component;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
 import com.placecube.nhs.talentdashboard.web.constants.PortletKeys;
 
-@Component(immediate = true, property = { "com.liferay.fragment.entry.processor.portlet.alias=talentdashboard", "com.liferay.portlet.css-class-wrapper=portlet-talentdashboard",
+@Component(immediate = true, property = { "com.liferay.fragment.entry.processor.portlet.alias=talentdashboard", "com.liferay.portlet.css-class-wrapper=portlet-nhs-maximized portlet-talentdashboard",
 		"com.liferay.portlet.display-category=category.nhs", "com.liferay.portlet.instanceable=false", "javax.portlet.resource-bundle=content.Language", "javax.portlet.init-param.template-path=/",
 		"com.liferay.portlet.header-portlet-css=/css/main.css", "javax.portlet.security-role-ref=power-user,user", "javax.portlet.name=" + PortletKeys.TALENT_DASHBOARD }, service = Portlet.class)
 public class TalentDashboardPortlet extends MVCPortlet {
--- com.placecube.nhs.user.profile.web/src/main/java/com/placecube/nhs/user/profile/portlet/professionalbodies/UserProfileProfessionalBodiesPortlet.java
+++ com.placecube.nhs.user.profile.web/src/main/java/com/placecube/nhs/user/profile/portlet/professionalbodies/UserProfileProfessionalBodiesPortlet.java
@@ -8,7 +8,7 @@ import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
 import com.placecube.nhs.user.profile.constants.PortletKeys;
 
 @Component(immediate = true, property = { "com.liferay.fragment.entry.processor.portlet.alias=user-profile-professional-bodies",
-		"com.liferay.portlet.css-class-wrapper=portlet-user-profile portlet-user-profile-professional-bodies", "com.liferay.portlet.display-category=category.nhs",
+		"com.liferay.portlet.css-class-wrapper=portlet-nhs-maximized portlet-user-profile portlet-user-profile-professional-bodies", "com.liferay.portlet.display-category=category.nhs",
 		"com.liferay.portlet.instanceable=false", "javax.portlet.resource-bundle=content.Language", "javax.portlet.init-param.template-path=/", "javax.portlet.security-role-ref=power-user,user",
 		"javax.portlet.name=" + PortletKeys.USER_PROFILE_PROFESSIONAL_BODIES, "javax.portlet.init-param.add-process-action-success-action=false" }, service = Portlet.class)
 public class UserProfileProfessionalBodiesPortlet extends MVCPortlet {
